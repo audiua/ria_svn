5211c865288e9708b94b30d9c70083935ac79cf9
Kostya <audiua@yandex.ru>
2016-03-16 00:07:59 +0000

    fix

--- lesson2/index.js
+++ lesson2/index.js
@@ -5,22 +5,18 @@ let http = require("http"),
     Counter = require('./node_modules/counter');
 
 let server = http.createServer(function handler(req, res, next){
+    let path = url.parse(req.url).pathname;
+
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-
-    let path = url.parse(req.url).pathname,
-        res_text = '';
-
     if(path == '/index.html'){
-        res_text = 'Привет Мир <br><br><a href="/count.html">count.html</a>';
+        res.end('Привет Мир <br><br><a href="/count.html">count.html</a>');
         Counter.setCount();
     } else if(path == '/count.html') {
-        res_text = 'Количество просмотров = '+Counter.getCount()+'<br><br><a href="/index.html">index.html</a>';
+        res.end('Количество просмотров = '+Counter.getCount()+'<br><br><a href="/index.html">index.html</a>');
     } else {
         res.statusCode = 404;
-        res_text = 'Page not found!<br><br><a href="/index.html">index.html</a>';
+        res.end('Page not found!<br><br><a href="/index.html">index.html</a>');
     }
-
-    res.end(res_text);
 });
 
 server.listen(3000);
